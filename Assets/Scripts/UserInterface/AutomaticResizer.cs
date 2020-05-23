1796e258729e6b75f8be64301242f751a5710fac
BadKarmaZen <badkarma.zen@gmail.com>
2020-05-23 15:32:50 +0200

    Improved Menu UI

---
+++ Assets/Scripts/UserInterface/AutomaticResizer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticResizer : MonoBehaviour
+{
+  public float childHeight;
+  public float childWidth;
+  public bool verticalMode;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    AdjustSize();
+  }
+
+  public void AdjustSize()
+  {
+    var size = GetComponent<RectTransform>().sizeDelta;
+
+    if (verticalMode)
+    {
+      size.y = this.transform.childCount * childHeight;
+    }
+    else
+    {
+      size.x = this.transform.childCount * childWidth;
+    }    
+
+    GetComponent<RectTransform>().sizeDelta = size;
+  }
+}
---
+++ Assets/Scripts/UserInterface/AutomaticResizerUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AutomaticResizer))]
+public class AutomaticResizerUI : Editor
+{
+  public override void OnInspectorGUI()
+  {
+    DrawDefaultInspector();
+
+    if(GUILayout.Button("Adjust Size"))
+    {
+      (target as AutomaticResizer)?.AdjustSize();
+    }
+  }
+}
